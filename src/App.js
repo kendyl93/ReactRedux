2299fff00b46acdd7bdd936d66c95e1424bca84d
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2020-01-22 20:00:47 +0100

    Added scss loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.scss';
 
 class App extends React.Component {
   constructor() {
     super();
   }
   render() {
-    return <div>REACT</div>;
+    return <h1>REACT</h1>;
   }
 }
 
