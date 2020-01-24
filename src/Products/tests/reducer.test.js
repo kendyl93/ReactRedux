0adea85ca8c1acd634d31b2e61aa6100d4b4fd60
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2020-01-24 11:32:33 +0100

    Added selectors tests

--- src/Products/tests/reducer.test.js
+++ src/Products/tests/reducer.test.js
@@ -1,4 +1,9 @@
-import { productsReducer } from '../reducer';
+import {
+  productsReducer,
+  getProducts,
+  getProductsPending,
+  getProductsError
+} from '../reducer';
 import {
   FETCH_PRODUCTS_SUCCESS,
   FETCH_PRODUCTS_ERROR,
@@ -53,5 +58,37 @@ describe('reducer', () => {
     };
 
     expect(productsReducer(state, pendingAction)).toEqual(expected);
+  });
+});
+
+describe('selectors', () => {
+  it('getProducts', () => {
+    const state = {
+      products: { milk: 'milk', soup: 'soup' },
+      pending: true,
+      error: false
+    };
+
+    expect(getProducts(state)).toEqual({ milk: 'milk', soup: 'soup' });
+  });
+
+  it('getProductsPending', () => {
+    const state = {
+      products: { milk: 'milk', soup: 'soup' },
+      pending: true,
+      error: false
+    };
+
+    expect(getProductsPending(state)).toEqual(true);
+  });
+
+  it('getProductsError', () => {
+    const state = {
+      products: { milk: 'milk', soup: 'soup' },
+      pending: true,
+      error: false
+    };
+
+    expect(getProductsError(state)).toEqual(false);
   });
 });
